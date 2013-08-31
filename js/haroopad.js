2d4adf1057a9041980e5e9a121bc049630605a79
rhiokim <rhio.kim@gmail.com>
2013-08-31 22:04:35 +0900

    dialog close & add picker component image search, youtube

--- js/haroopad.js
+++ js/haroopad.js
@@ -1,6 +1,15 @@
 window.ee = new EventEmitter();
 
+var lang = navigator.language.split('-');
+    lang.pop();
+    lang = lang.join('-');
+
 var googlePickerCallback = function(res) {
+    if (res.action === google.picker.Action.CANCEL) {
+        ee.emit('close');
+        return;
+    }
+
 	if (res.action !== google.picker.Action.PICKED) return;
 
 	ee.emit('picked', res);
@@ -8,14 +17,16 @@ var googlePickerCallback = function(res) {
 
 var createBuilder = function() {
     var builder = new google.picker.PickerBuilder().
+        setLocale(lang).
         enableFeature(google.picker.Feature.MULTISELECT_ENABLED).
         addView(google.picker.ViewId.PHOTOS).
         addView(google.picker.ViewId.DOCS).
-        addView(google.picker.ViewId.DOCS_VIDEOS).
         addView(new google.picker.PhotosView().setType('camerasync')).
         addView(google.picker.ViewId.PHOTO_UPLOAD).
         // addView(google.picker.ViewId.WEBCAM).
         addView(google.picker.ViewId.MAPS).
+        addView(google.picker.ViewId.IMAGE_SEARCH).
+        addView(google.picker.ViewId.YOUTUBE).
         addView(google.picker.ViewId.VIDEO_SEARCH);
 
     return builder;
@@ -33,14 +44,12 @@ var launchGooglePicker = function () {
     });
 };
 
-// $(document.body).ready(function() {
 function init() {
 	if (typeof window.google === 'undefined') {
 		var script = document.createElement('script');
 		var head = document.getElementsByTagName('head')[0];
 		script.src = "http://www.google.com/jsapi";
 		head.appendChild(script);
-        // $('<script src="http://www.google.com/jsapi"></script>').appendTo(document.getElementsByTagName('head')[0]);
        
         var googleTimer = setInterval(function () {
             if (typeof window.google === 'undefined') return;
